49baf7d51d2cd68263db622763b2fca31d310d29
gutcod <gutsanomania@gmail.com>
2020-07-25 02:20:31 +0300

    add database

--- Project/cui-api/server.js
+++ Project/cui-api/server.js
@@ -7,7 +7,7 @@ const knex = require("knex");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-lively-97634",
     user: "",
     password: "",
     database: "cui-test",
